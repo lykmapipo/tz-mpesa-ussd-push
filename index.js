97d248011faa5e4b89b8aa830c15eea187561613
lykmapipo <lallyelias87@gmail.com>
2019-02-07 20:04:37 +0300

    feat: implement parseTransactionResult

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 
 /* dependencies */
 const _ = require('lodash');
+const moment = require('moment');
 const xml2js = require('xml2js');
 const {
   parse: xmlToJson,
@@ -11,6 +12,7 @@ const {
 
 
 /* constants */
+const DATE_FORMAT = 'YYYYMMDD HHmmss';
 const REQUEST_HEADER_TAG = 'envelope.header';
 const REQUEST_DATA_TAG = 'envelope.body.getGenericResult.request.dataItem';
 
@@ -20,7 +22,7 @@ const REQUEST_DATA_TAG = 'envelope.body.getGenericResult.request.dataItem';
  * @description Human readable country code of a payment processing entity.
  * @since 0.1.0
  * @version 0.1.0
- * @instance
+ * @public
  */
 const country = 'TZ';
 
@@ -30,7 +32,7 @@ const country = 'TZ';
  * @description Human readable name of a payment processing entity.
  * @since 0.1.0
  * @version 0.1.0
- * @instance
+ * @public
  */
 const provider = 'Vodacom';
 
@@ -40,7 +42,7 @@ const provider = 'Vodacom';
  * @description Human readable supported method of a payment.
  * @since 0.1.0
  * @version 0.1.0
- * @instance
+ * @public
  */
 const method = 'Mobile Money';
 
@@ -50,7 +52,7 @@ const method = 'Mobile Money';
  * @description Human readable supported channel of a payment.
  * @since 0.1.0
  * @version 0.1.0
- * @instance
+ * @public
  */
 const channel = 'MPESA';
 
@@ -60,7 +62,7 @@ const channel = 'MPESA';
  * @description Human readable supported mode of a payment.
  * @since 0.1.0
  * @version 0.1.0
- * @instance
+ * @public
  */
 const mode = 'USSD Push';
 
@@ -70,9 +72,43 @@ const mode = 'USSD Push';
  * @description Currency accepted for payment.
  * @since 0.1.0
  * @version 0.1.0
- * @instance
+ * @public
  */
 const currency = 'TZS';
+
+
+/**
+ * @function transformValue
+ * @name transformValue
+ * @description Transform data item value to js object
+ * @param {Object} item data item
+ * @return {Object} transformed value
+ * @since 0.1.0
+ * @version 0.1.0
+ * @private
+ * @example
+ * const { transformValue } = require('@lykmapipo/tz-mpesa-ussd-push');
+ * transformValue(item);
+ */
+const transformValue = item => {
+  // ensure item
+  let { name, type, value } = _.merge({}, { value: undefined }, item);
+
+  // transform date
+  if (name === 'Date' && value) {
+    value = moment(value, DATE_FORMAT).toDate();
+    return value;
+  }
+
+  // transform string
+  if (type === 'String' && value) {
+    value = value === 'null' ? undefined : value;
+    return value;
+  }
+
+  // always return value
+  return value;
+};
 
 
 /**
@@ -84,7 +120,7 @@ const currency = 'TZS';
  * @return {Object|Error} parsed request or error
  * @since 0.1.0
  * @version 0.1.0
- * @instance
+ * @public
  * @example
  * const { parseRequest } = require('@lykmapipo/tz-mpesa-ussd-push');
  * parseRequest(xml, (error, request) => { ... });
@@ -108,7 +144,8 @@ const parseRequest = (xml, done) => {
     const items = _.get(json, REQUEST_DATA_TAG, []);
     const body = _.reduce(items, (accumulator, item) => {
       const value = {};
-      value[_.camelCase(item.name)] = item.value;
+      const key = _.camelCase(item.name);
+      value[key] = transformValue(item);
       return _.merge({}, accumulator, value);
     }, {});
 
@@ -118,10 +155,30 @@ const parseRequest = (xml, done) => {
 };
 
 
+/**
+ * @function parseTransactionResult
+ * @name parseTransactionResult
+ * @description Parse and convert ussd push xml result to json
+ * @param {String} xml valid xml payload
+ * @param {Function} done callback to invoke on success or errorH
+ * @return {Object|Error} parsed result or error
+ * @since 0.1.0
+ * @version 0.1.0
+ * @public
+ * @example
+ * const { parseTransactionResult } = require('@lykmapipo/tz-mpesa-ussd-push');
+ * parseTransactionResult(xml, (error, request) => { ... });
+ */
+const parseTransactionResult = (xml, done) => parseRequest(xml, done);
+
+
 const buildRequest = (json, done) => {
   // prepare build options
   jsonToXml(json, done);
 };
+
+// const buildLoginRequest;
+// const buildTransactionRequest;
 
 
 /* expose */
@@ -133,5 +190,6 @@ module.exports = exports = {
   mode,
   currency,
   parseRequest,
+  parseTransactionResult,
   buildRequest
 };
--- test/index.spec.js
+++ test/index.spec.js
@@ -61,4 +61,35 @@ describe('tz mpesa ussd push', () => {
       done(error, request);
     });
   });
+
+  it('should parse transaction result to json', (done) => {
+    const xmlPath = `${__dirname}/fixtures/transaction_result.xml`;
+    const xml = readFileSync(xmlPath, 'UTF-8');
+    parseRequest(xml, (error, result) => {
+      expect(error).to.not.exist;
+      expect(result).to.exist;
+      const { header, body } = result;
+      expect(header).to.exist;
+      expect(header).to.be.an('object');
+      expect(header).to.be.eql({ eventId: 1 });
+      expect(body).to.exist;
+      expect(body).to.be.an('object');
+      expect(body).to.be.eql({
+        resultType: undefined,
+        resultCode: undefined,
+        resultDesc: undefined,
+        transactionStatus: 'USSDCallbackCancel',
+        originatorConversationId: undefined,
+        conversationId: undefined,
+        transId: undefined,
+        businessNumber: 888888,
+        currency: 'TZS',
+        amount: 1500,
+        date: new Date('2017-12-04T16:01:47.000Z'),
+        thirdPartyReference: 'E5FK3170',
+        insightReference: '5F8648318CD95BC3E0531600980A264E'
+      });
+      done(error, result);
+    });
+  });
 });
